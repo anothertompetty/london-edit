363b8f0ecd4ec9804ddab032dcd1d5acf4b9b265
Tom Petty <hello@anothertompetty.com>
2015-01-08 21:11:58 +0000

    adding location detection, radius, and error handling

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,19 +1,56 @@
 $(document).on('ready', function() {
 
-  console.log('scripts loading!');
+  // Get user's location
 
-  // Foursquare client ID VN4Y3OWHFGUGYXOGB2KDS4A50PZP31RG1DNSDWDGK0NVW0ZZ
-  // Foursquare App secret IZQHG5ZVLA3M220YAJAOJN4O2RR45BBGWXJT2JXOJU00JBHU
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log('this didn\'t work');
+    }
+  };
 
-  function searchFoursquare(searchTerm) {
+  function showPosition(position) {
+    console.log('Latitude: ' + position.coords.latitude + ', Longitude: ' + position.coords.longitude); 
+  };
 
+  console.log(getLocation());
+
+  // Api GET function
+
+  // function debug() {
+    
+  //   $.getJSON('https://api.foursquare.com/v2/venues/explore?client_id=VN4Y3OWHFGUGYXOGB2KDS4A50PZP31RG1DNSDWDGK0NVW0ZZ&client_secret=IZQHG5ZVLA3M220YAJAOJN4O2RR45BBGWXJT2JXOJU00JBHU&v=20130815&ll=51.562771299999994,-0.0792068&radius=100&query=sushi', function(theResponse) {
+
+  //     if (theResponse.response.hasOwnProperty('warning')) {
+  //       console.log(theResponse.response.warning.text);
+  //     } else {
+  //       console.log(theResponse);
+  //     };
+
+  //   });
+
+  // };
+
+  // debug();
+
+  function searchFoursquare(exactPosition, radius, searchTerm) {
+
+    var baseUrl = 'https://api.foursquare.com/v2/venues/explore'
     var token = 'VN4Y3OWHFGUGYXOGB2KDS4A50PZP31RG1DNSDWDGK0NVW0ZZ';
     var secret = 'IZQHG5ZVLA3M220YAJAOJN4O2RR45BBGWXJT2JXOJU00JBHU';
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + token + '&client_secret=' + secret + '&v=20130815&near=london&query=' + searchTerm;
+    var url = baseUrl + 
+              '?client_id=' + token + 
+              '&client_secret=' + secret + 
+              '&v=20130815' + 
+              '&ll=' + exactPosition + 
+              '&radius=' + radius + 
+              '&query=' + searchTerm;
     
     $.getJSON(url, function(json) {
 
-      var venueList = json.response.groups[0].items;
+      var response = json.response
+      var venueList = response.groups[0].items;
       var topVenue = venueList[0];
       var venue = {
         name : topVenue.venue.name,
@@ -22,24 +59,36 @@ $(document).on('ready', function() {
         icon : topVenue.venue.categories[0].icon.prefix 
       };
 
-      console.log(topVenue);
+      // Check to make sure there's a result
+      
+      if (response.hasOwnProperty('warning')) { 
 
-      $('.top-result').fadeIn();
-      $('.result strong').text(venue.name);
-      $('.location').text(venue.address);
-      $('.url').text(venue.url);
-      $('.category').html('<img src="' + venue.icon + '64.png">');
+        $('.error').fadeIn().text(response.warning.text);
+
+      } else {
+
+        $('.top-result').fadeIn();
+        $('.result strong').text(venue.name);
+        $('.location').text(venue.address);
+        $('.url').text(venue.url);
+        $('.category').html('<img src="' + venue.icon + '64.png">');
+
+      };
 
     });
 
   };
 
+  // Run above function on form submit
+
   $('#search-form').on('submit', function() {
 
+    var latAndLong = '51.5627849,-0.0791954';
+    var radiusInMeters = '100';
     var searchQuery = $('.search-field').val().trim();
-    searchFoursquare(searchQuery);
-    console.log(searchQuery);
 
+    searchFoursquare(latAndLong, radiusInMeters, searchQuery);
+    
     return false;
 
   });
